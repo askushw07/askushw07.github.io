a4e09a0ca93288da1ffe73922251a12e0a630fa3
Alok Singh <askushw07@gmail.com>
2023-09-22 11:51:50 +0530

    react portfolio

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -5,6 +5,7 @@ import Social from '../home/Social';
 const Footer = () => {
   return (
     <footer className="footer">
+
       <div className="container">
         <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
         <div className="social-icons">
